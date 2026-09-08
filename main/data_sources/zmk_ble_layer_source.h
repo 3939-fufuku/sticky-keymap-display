fb946b3e0359e59a2cf22f15a83832ccf432bac9
3939-fufuku <fukui39tus@gmail.com>
2026-09-08 14:30:13 +0900

    Add Sticky and Nickey battery status

--- main/data_sources/zmk_ble_layer_source.h
+++ main/data_sources/zmk_ble_layer_source.h
@@ -10,12 +10,14 @@ class ZmkBleLayerSource final : public LayerSource {
 public:
     esp_err_t start(LayerChangedCallback callback, void *context) override;
     uint8_t initial_layer() const override { return current_layer_; }
+    int battery_percent() const { return battery_percent_; }
 
     void handle_layer(uint8_t layer);
+    void handle_battery(uint8_t percent);
 
 private:
     LayerChangedCallback callback_ = nullptr;
     void *context_ = nullptr;
     volatile uint8_t current_layer_ = 0;
+    volatile int battery_percent_ = -1;
 };
-

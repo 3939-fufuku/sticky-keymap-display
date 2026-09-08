96c902cdbe8a0b79430d6f4741719833d0e4f6ba
3939-fufuku <fukui39tus@gmail.com>
2026-09-09 08:20:10 +0900

    Expose separate Nickey battery values

--- main/data_sources/zmk_ble_layer_source.h
+++ main/data_sources/zmk_ble_layer_source.h
@@ -10,14 +10,16 @@ class ZmkBleLayerSource final : public LayerSource {
 public:
     esp_err_t start(LayerChangedCallback callback, void *context) override;
     uint8_t initial_layer() const override { return current_layer_; }
-    int battery_percent() const { return battery_percent_; }
+    int left_battery_percent() const { return left_battery_percent_; }
+    int right_battery_percent() const { return right_battery_percent_; }
 
     void handle_layer(uint8_t layer);
-    void handle_battery(uint8_t percent);
+    void handle_batteries(uint8_t left_percent, uint8_t right_percent);
 
 private:
     LayerChangedCallback callback_ = nullptr;
     void *context_ = nullptr;
     volatile uint8_t current_layer_ = 0;
-    volatile int battery_percent_ = -1;
+    volatile int left_battery_percent_ = -1;
+    volatile int right_battery_percent_ = -1;
 };
